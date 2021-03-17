3fd096eff1b4a847cf5f3712234393ebb712551c
shendi <shendi@gmail.com>
2021-03-17 04:09:07 +0100

    sub product stock feature done

--- functions/createOrder.js
+++ functions/createOrder.js
@@ -1,112 +1,172 @@
 const admin = require('firebase-admin');
 const db = admin.firestore();
 const stripeHelper = require('./stripeHelper.js');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({ origin: true });
 
 exports.createOrderHandler = ((req, res) => {
     cors(req, res, () => {
         var body = req.body;
         let orderRef = db.collection('Orders').doc();
         let amount = body.amount;
-        let token  = body.token;
+        let token = body.token;
         let cod = body.cod;
-    
+
         body = replaceDates(body);
         body.id = orderRef.id;
         body.createdAt = admin.firestore.FieldValue.serverTimestamp()
-    
+
         const cartItems = body.products == null ? [] : body.products;
         var getProducts = [];
         cartItems.forEach(item => {
             const req = db.collection('Products').doc(item.product.id).get();
             getProducts.push(req);
         });
-    
+
         let batch = db.batch();
         Promise.all(getProducts)
-            .then(snaps => {
+            .then(async (snaps) => {
                 var outOfStock;
+                var tmpBackup = []
                 for (let index = 0; index < snaps.length; index++) {
                     const snap = snaps[index];
                     const product = snap.data();
-                    
-                    if (!product){
+
+                    if (!product) {
+                        console.log("Some of the products is null")
                         res.status(404).send({
                             success: false,
                             message: "您购物车中的某些产品不可用。您的购物车现在将刷新",
                             messageEng: "Some of the products from your cart are not available. Your cart will be refreshed now"
-                        }); 
-                        return;
+                        });
+                        return "sent_res"
                     }
-    
-                    if (isOutOfStock(cartItems, product)) {
-                        outOfStock = product
+
+                    // check out of stock
+                    const cartItemList = cartItems.filter(item => {
+                        return product.id === item.product.id
+                    });
+
+                    let tmpFilterdBackup = tmpBackup.filter(item => { return product.id == item})
+                    if (tmpFilterdBackup.length == 0) {
+                        
+                        let total_qty = 0
+                        cartItemList.forEach(cartitem => {
+                            total_qty = total_qty + parseInt(cartitem.quantity);
+                        });
+
+                        let pStock = parseInt(product.stock);
+                        if (pStock - total_qty < 0) { // this product is out of stock
+                            outOfStock = product
+
+                            console.log("out of stock of main product : ", product.title)
+                            break
+                        } else {
+                            let productRef = db.collection('Products').doc(product.id);
+                            let stockUpdate = { stock: (pStock - total_qty).toString() };
+                            batch.update(productRef, stockUpdate);
+                        }
+                        tmpBackup.push(product.id)
+                    }
+
+                    // get all sub products list
+                    var subProductIds = [];
+                    cartItemList.forEach(cartitem => {
+                        cartitem.subProducts.forEach(subpitem => {
+                            let found = false
+                            for(let subp_id = 0; subp_id < subProductIds.length; subp_id ++) {
+                                if(subProductIds[subp_id].product_id == subpitem.id) {
+                                    found = true
+                                    subProductIds[subp_id].cnt = subProductIds[subp_id].cnt + cartitem.quantity
+                                }
+                            }
+                            if(found == false) {
+                                subProductIds.push({
+                                    product_id : subpitem.id,
+                                    cnt : cartitem.quantity
+                                })
+                            }
+                        })
+                    });
+
+                    console.log("subProductIds", subProductIds)
+
+                    for(let sub_id = 0; sub_id < subProductIds.length; sub_id ++ ) {
+                        let subP_ref = await db.collection('Products').doc(product.id).collection("sub_products").doc(subProductIds[sub_id].product_id).get();
+                        if (subP_ref.data() == null) {
+                            console.log("Some of the sub products is null")
+                            res.status(404).send({
+                                success: false,
+                                message: "您购物车中的某些子产品不可用。您的购物车现在将刷新",
+                                messageEng: "Some of the sub products from your cart are not available. Your cart will be refreshed now"
+                            });
+                            return "sent_res"
+                        }
+                        else {
+                            let subp_stock = parseInt(subP_ref.data().stock);
+                            if (subp_stock - subProductIds[sub_id].cnt < 0) { // this product is out of stock
+                                outOfStock = subP_ref.data()
+                                console.log("out of stock of sub product : ", outOfStock.title)
+                                break
+                            } else {
+                                let subProductRef = db.collection('Products').doc(product.id).collection("sub_products").doc(subProductIds[sub_id].product_id);
+                                let subPStockUpdate = { stock: (subp_stock - subProductIds[sub_id].cnt).toString() };
+                                batch.update(subProductRef, subPStockUpdate);
+                            }
+                        }
+                    }
+                    if (outOfStock) {
                         break;
-                    } else {
-                        const cartItem = cartItems.filter(item => {
-                            return product.id === item.product.id
-                        })[0];
-                        let productRef = db.collection('Products').doc(product.id);
-                        let pStock = parseInt(product["stock"]);
-                        let cartItemQty = parseInt(cartItem["quantity"]);
-                        let stockUpdate = { stock: (pStock - cartItemQty).toString() };
-                        batch.update(productRef, stockUpdate);
                     }
                 }
-                
+
                 if (outOfStock) {
+                    console.log("outOfStock")
                     res.status(500).send({
                         success: false,
-                        message:    "訂單失敗," + outOfStock.title + "暫時售罄",
+                        message: "訂單失敗," + outOfStock.title + "暫時售罄",
                         messageEng: "Order Failed, " + outOfStock.title + " is out of stock"
-                    }); 
+                    });
+                    return "sent_res"
                 } else {
                     if (cod == true) {
+                        console.log("cod true")
                         batch.set(orderRef, body);
-                        return batch.commit();
-                    }else{
-                        return stripeHelper.createChargeWith(token,amount,body.id);
+                        return batch.commit()
+                    } else {
+                        console.log("createChargeWith")
+                        return stripeHelper.createChargeWith(token, amount, body.id);
                     }
                 }
-            }).then( chargeObj => {
-                if (cod == true){
-                    console.log('Order created');
+            }).then(chargeObj => {
+                if(chargeObj == "sent_res") {
+                    console.log("sent res 1")
+                    return chargeObj
+                }
+
+                if (cod == true) {
+                    console.log('Order created 1');
                     res.status(200).send({ success: true, orderId: body.id, message: '訂單已確認', messageEng: 'Order created' });
-                }else{
+                    return "sent_res"
+                } else {
                     body.chargeId = chargeObj.id;
                     batch.set(orderRef, body);
-                    return batch.commit();        
+                    return batch.commit();
                 }
             }).then(ref => {
-                console.log('Order created');
-                res.status(200).send({ success: true, orderId: body.id,  message: '訂單已確認', messageEng: 'Order created' });
+                if(ref == "sent_res") {
+                    console.log("sent res 2")
+                    return 
+                }
+                console.log('Order created 2');
+                res.status(200).send({ success: true, orderId: body.id, message: '訂單已確認', messageEng: 'Order created' });
             })
             .catch(err => {
                 console.log('Error', err);
-                res.status(404).send({ success: false, message: "Order creating failed",  error : err });
+                res.status(404).send({ success: false, message: "Order creating failed", error: err });
             });
     })
 });
 
-
-function isOutOfStock(cartItems, product) {
-    
-    const cartItem = cartItems.filter(item => {
-        return product.id === item.product.id
-    });
-    if(cartItem.length < 1) 
-    {
-        return false
-    }
-    let pStock = parseInt(product.stock);
-    let cartItemQty = parseInt(cartItem[0].quantity);
-
-    if (pStock - cartItemQty < 0) {
-        return true
-    } else {
-        return false
-    }
-}
 
 function replaceDates(obj) {
     for (var property in obj) {
@@ -117,7 +177,7 @@ function replaceDates(obj) {
                     let timeStamp = admin.firestore.Timestamp.fromMillis(miliseconds);
                     obj[property] = timeStamp;
                 }
-                catch(e) {
+                catch (e) {
                     obj[property] = admin.firestore.Timestamp.fromDate(new Date());
                 }
             }
@@ -127,28 +187,28 @@ function replaceDates(obj) {
                     let timeStamp = admin.firestore.Timestamp.fromMillis(miliseconds);
                     obj[property] = timeStamp;
                 }
-                catch(e) {
+                catch (e) {
                     obj[property] = admin.firestore.Timestamp.fromDate(new Date());
                 }
             }
-            if (property == "customer"){
+            if (property == "customer") {
                 obj[property] = replaceDates(obj[property]);
             }
-            if (property == "coupon"){
+            if (property == "coupon") {
                 obj[property] = replaceDates(obj[property]);
             }
-            if (property == "products"){
+            if (property == "products") {
                 var cartItems = obj[property];
-                for (var i = 0; i < cartItems.length;i++){
+                for (var i = 0; i < cartItems.length; i++) {
                     var p = cartItems[i].product;
                     p = replaceDates(p);
-                    cartItems[i].product = p;    
+                    cartItems[i].product = p;
 
                     var subProducts = cartItems[i].subProducts
-                    for (var j = 0; j < subProducts.length; j++){
+                    for (var j = 0; j < subProducts.length; j++) {
                         var p1 = subProducts[j];
                         p1 = replaceDates(p1);
-                        subProducts[j] = p1;    
+                        subProducts[j] = p1;
                     }
                     cartItems[i].subProducts = subProducts
                 }
